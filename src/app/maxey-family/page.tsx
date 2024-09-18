85a16d9889eea6adfc07b21b28bb47118b3c402c
markbokeeffe <mark.okeeffe@gmail.com>
2024-09-18 12:41:26 +0100

    Imagery

--- src/app/maxey-family/page.tsx
+++ src/app/maxey-family/page.tsx
@@ -15,6 +15,14 @@ export default function Page() {
           /> 
           <p><span style={{ color: 'darkred' }} className="font-bold">The Maxey Family was a well-known family that lived on what was then known as Johnson Street (now Gladstone Street) in Clonmel in the mid-nineteenth century.</span></p>
           <p>William Maxey was a publican during the 1840s and 1850s who had his premises at 11 Johnson Street and he was married to Bridget Kelly.  William was quite prominent in Clonmel and was listed as a voter for the town.  He also held the occuptation of cooper, which his sons would follow him into.</p>
+          <Image
+            src="/wmmaxey_business.png"
+            alt="William Maxey business"
+            className="dark:invert border-4 border-black opacity-100 z-10 mt-10 lg:float-right lg:ml-4"
+            width={300}
+            height={24}
+            priority
+          /> 
           <p>William was born around 1799 and he married Bridget Kelly some time before 1833.  Together, they had at least five children:</p>
           <ul>
             <li>- William (1833 -)</li>
@@ -23,7 +31,16 @@ export default function Page() {
             <li>- Joseph (1843 -)</li>
             <li>- Mary Anne (1844 - 1895)</li>
           </ul>
-          <p>Some time between the 1850s and the mid-1860s, William Maxey fell on hard times.  When his son, Joseph, was imprisoned in 1866-67, pleas for his release were made on the basis that his family had fallen into destitution, including his father William, who was dependent upon him.  Whilst these pleas fell upon deaf ears, William passed away in the workhouse in Clonmel at the age of 67 from a bladder infection.  Bridget outlived Wililam.  However, it is known when she died.</p>
+          <p>Some time between the 1850s and the mid-1860s, William Maxey fell on hard times.  When his son, Joseph, was imprisoned in 1866-67, pleas for his release were made on the basis that his family had fallen into destitution, including his father William, who was dependent upon him.</p>
+          <Image
+            src="/williamMaxeyPlea.png"
+            alt="William Maxey Plea"
+            className="dark:invert border-4 border-black opacity-100 z-10 mt-4 lg:float-left lg:mr-4"
+            width={400}
+            height={24}
+            priority
+          /> 
+          <p>Whilst these pleas fell upon deaf ears, William passed away in the workhouse in Clonmel at the age of 67 from a bladder infection.  Bridget outlived Wililam.  However, it is known when she died.</p>
         </ContentPage>
     </>;
   }
