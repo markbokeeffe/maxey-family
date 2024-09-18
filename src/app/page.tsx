58b86d762b76dbc118f48a929f9b960897fff279
markbokeeffe <mark.okeeffe@gmail.com>
2024-09-18 10:51:38 +0100

    Home background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,21 @@ import Navigation from "./components/Navigation";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center lg:justify-between p-0">
-      <Navigation active='timothyMaxey' isHome={true} />
-      <div className="w-full max-w-4xl flex justify-evenly relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] flex-col sm:flex-row pb-24" >
+      <Navigation isHome={true} />
+      <div className="home-container w-full max-w-4xl flex justify-evenly relative z-[-1] flex place-items-center flex-col sm:flex-row pb-24" >
             <Image
-              src="/mainguard_clonmel.jpg"
-              alt="Main Guard, Clonmel"
-              className="absolute top-0 left-0 opacity-20"
-              sizes="100vw"
-              width="0"
-              height="0"
-              style={{
-                width: '100%',
-                height: 'auto',
-              }}
+              src="/joseph_maxey.jpg"
+              alt="Joseph Maxey"
+              className="dark:invert border-4 border-black opacity-100 z-10 mt-10 mb-6 lg:mb-0"
+              width={300}
+              height={24}
+              priority
             />
-              <Image
-                src="/joseph_maxey.jpg"
-                alt="Joseph Maxey"
-                className="dark:invert border-4 border-black opacity-100 z-10 mt-10 mb-6 lg:mb-0"
-                width={300}
-                height={24}
-                priority
-              />
-              <div className="flex-col text-center">
-                <h2 className="text-3xl font-extrabold">The Maxey Family</h2>
-                <h2 className="text-2xl font-bold">of Clonmel</h2>
-                <p className="italic mt-4">~ 19th Century Rebels ~</p>
-              </div>
+            <div className="flex-col text-center">
+              <h2 className="text-3xl font-extrabold">The Maxey Family</h2>
+              <h2 className="text-2xl font-bold">of Clonmel</h2>
+              <p className="italic mt-4">~ 19th Century Fenians ~</p>
+            </div>
       </div>
 
       <div className="mb-32 mt-14 hidden lg:grid text-center lg:mb-0 lg:w-full lg:max-w-7xl lg:grid-cols-4 lg:text-left  border-t-2 border-black">
