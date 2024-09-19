83cc4edac13071240b0876c26ccbdf79adf53cc0
markbokeeffe <mark.okeeffe@gmail.com>
2024-09-19 20:27:32 +0100

    Logo logic

--- src/app/components/Logo/index.tsx
+++ src/app/components/Logo/index.tsx
@@ -1,7 +1,13 @@
 "use client"
+import clsx from 'clsx';
 
-export const Logo = () => 
-      <a href="/" style={{ color: 'darkred', fontFamily: 'fantasy' }} className="no-underline font-extrabold text-3xl leading-3 ml-4">
+type LogoProps = {
+      isHome?: boolean;
+  };
+  
+
+export const Logo = ({ isHome = false }: LogoProps ) => 
+      <a href="/" style={{ color: 'darkred', fontFamily: 'fantasy' }} className={clsx(isHome && "lg:hidden", "no-underline font-extrabold text-3xl leading-3 ml-4")}>
             Maxey
       </a>;
 
